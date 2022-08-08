3b09eb7228943744fdb33dcf9c7df834b56a85c3
Aryan Pal <aryanpalongraph@gmail.com>
2022-08-08 19:04:19 +0530

    ➕ Added Seed Files

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -2,13 +2,15 @@ class Api::V1::LocationsController < ApplicationController
   before_action :set_location
   
   def show
-    # This will render the hash
+    # This will render the d
     # render json:
     # {
     #   id: @location.id,
     #   name: @location.name
 
     # }
+    debugger
+    render "show.json.jbuilder"
   end
 
   private
